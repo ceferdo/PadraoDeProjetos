4d08097dec7484edca324c165789814d5e3b08ef
ceferdo <ceferdo@gmail.com>
2019-08-10 14:07:44 -0300

    Update Composite.cs

--- Composite.cs
+++ Composite.cs
@@ -7,17 +7,13 @@ class Program
             IPessoa mae = new Mulher("Maria");
             IPessoa filho1 = new Homem("filho1");
             IPessoa filho2 = new Mulher("filho2");
- 
- 
+            
             mae.Filho(filho1);
             mae.Filho(filho2);
  
             filho2.Filho(new Homem("Carlos"));
  
- 
             Console.WriteLine(mae);
- 
- 
             Console.ReadKey();
         }
     }
@@ -38,7 +34,6 @@ interface IPessoa
             this.Nome = nome;
         }
  
-     
         public void Filho(IPessoa o)
         {
             Console.Write("não gera filho");
@@ -52,43 +47,32 @@ interface IPessoa
 
     class Mulher : IPessoa
     {
-        
         List filho;
- 
  
         public String Nome { get; set; }
         public int QtFilho { get; set; }
- 
  
         public Mulher(String nome)
         {
             this.Nome = nome;
             this.filho = new List();
         }
- 
- 
-        //Operation
+        
         public void Filho(IPessoa o)
         {
             o.QtFilho = this.QtFilho + 2;
             this.filho.Add(o);
         }
  
- 
-        //Operation
         public override string ToString()
         {
             String retorno = String.Format("{0}{1}\n",
                 new String(' ', this.QtFilho),
                 this.Nome);
- 
- 
             foreach (var item in this.conteudo)
             {
                 retorno += item;
             }
- 
- 
             return retorno;
         }
     }
